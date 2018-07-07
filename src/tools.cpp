beec3ff4f68f8cc720086f7f2fef8657ef3db08b
leclair-7 <andromeda.t@tutanota.com>
2018-07-06 22:06:53 -0500

    Put my RMSE (Eigen) function in tools, may have put a good Jacobian in

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,15 +11,67 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  	// made it different than 4 elements like in the class material; need it to adapt to whatever size it needs to be
+  	// also it seems as though different sensor packages with require different error calculations
+  	thesize = estimations.size();
+
+  	VectorXd rmse(thesize);
+  	for (int i=0 ; i < thesize; i++)
+  	{
+  		rmse << 0;
+  	}
+	//rmse << 0,0,0,0;
+  
+	if (estimations.size() != ground_truth.size() || estimations.size() == 0 )
+	{
+		cout<< "Invalid estimations of ground_truth size"<<endl;
+		return rmse;
+	}
+	//accumulate squared residuals
+	
+	VectorXd res(4);
+	VectorXd temp(4);
+	res<<0.0,0.0,0.0,0.0;
+
+	for(int i=0; i < estimations.size(); ++i){
+		temp = (estimations[i].array() - ground_truth[i].array() );
+		res = res.array() + temp.array() * temp.array();
+	}
+	
+	res = (1.0/estimations.size()) * res;
+
+	res = res.array().sqrt();
+
+	return res;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+	  /**
+	  TODO:
+	    * Calculate a Jacobian here.
+	  */
+	
+	MatrixXd Hj(3,4);
+	//recover state parameters
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	//TODO: YOUR CODE HERE 
+	if (px < .00001 || py < .00001){
+	    cout<<"Divide by zero error" <<endl;
+	    return Hj;}
+    Hj <<
+        px/pow(px*px+py*py,.5),py/pow(px*px+py*py,.5),0,0,
+        -1*py/(px*px+py*py),px/(px*px+py*py),0,0,
+        py*(vx*py - vy*px)/(pow((px*px+py*py),1.5)),
+        px * (vy*px-vx*py )  / (pow((px*px+py*py),1.5)),
+        px/pow(px*px+py*py,.5),py/pow(px*px+py*py,.5)
+        ;
+	//check division by zero
+	
+	//compute the Jacobian matrix
+
+	return Hj;
 }
